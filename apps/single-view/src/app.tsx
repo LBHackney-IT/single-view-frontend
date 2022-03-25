475bc555b1d138394a75e48ae5f76cfe0bcda662
Charli Wild <charli.wild@charlis-mbp.lan>
2022-03-25 15:40:58 +0000

    added state

--- apps/single-view/src/app.tsx
+++ apps/single-view/src/app.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { $auth } from "@mtfh/common/lib/auth";
 import { SearchView } from "./views/search";
 
 const App = (): JSX.Element => {
-  const [auth, setAuth] = useState($auth.getValue());
-
+  const [hasSearched, setHasSearched] = useState(false);
   return (
     <>
       <h1 className="lbh-heading-h1">Welcome to Single View 2.0</h1>
