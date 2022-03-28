2443faed02dfc02190be10fb32d83b3907c1cd09
el <elena@madetech.com>
2022-03-28 14:30:07 +0100

    make auth dev terrafrom refrence single view
    Co-authored-by: Jonathan Lewis <jonathan.lewis@madetech.com>

--- apps/auth/terraform/development/main.tf
+++ apps/auth/terraform/development/main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-housing-development"
+    bucket  = "terraform-state-corporate-development"
     encrypt = true
     region  = "eu-west-2"
-    key     = "services/t-and-l-auth-frontend/state"
+    key     = "services/single-view-auth-frontend/state"
   }
 }
 resource "aws_s3_bucket" "frontend-bucket-development" {
-  bucket = "lbh-housing-tl-auth-frontend-development.hackney.gov.uk"
+  bucket = "lbh-single-view-auth-frontend-development.hackney.gov.uk"
   acl    = "private"
   versioning {
     enabled = true
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "frontend-bucket-development" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://manage-my-home-development.hackney.gov.uk"]
+    allowed_origins = ["https://single-view.hackney.gov.uk"]
     expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
     max_age_seconds = 3000
   }
@@ -31,18 +31,18 @@ resource "aws_s3_bucket" "frontend-bucket-development" {
 module "cloudfront-development" {
   source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
   s3_domain_name = aws_s3_bucket.frontend-bucket-development.bucket_regional_domain_name
-  origin_id = "mtfh-t-and-l-auth-frontend"
+  origin_id = "single-view-auth-frontend"
   s3_bucket_arn = aws_s3_bucket.frontend-bucket-development.arn
   s3_bucket_id = aws_s3_bucket.frontend-bucket-development.id
-  orginin_access_identity_desc = "T&L auth frontend cloudfront identity"
+  orginin_access_identity_desc = "Single view auth frontend cloudfront identity"
   cname_aliases = []
   environment_name = "development"
   cost_code= "B0811"
-  project_name= "MTFH Tenants and Leaseholders"
+  project_name= "Single View Frontend"
   use_cloudfront_cert = true
 }
 resource "aws_ssm_parameter" "cdn" {
-  name  = "/housing-tl/development/auth-app-url"
+  name  = "/single-view/development/auth-app-url"
   type  = "String"
   value = "https://${module.cloudfront-development.cloudfront_domain_name}"
   overwrite = true
