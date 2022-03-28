f588f423066f017b9b9aed5122bea9df88d0b5a4
el <elena@madetech.com>
2022-03-28 15:43:28 +0100

    fix auth terraform

--- apps/auth/terraform/production/main.tf
+++ apps/auth/terraform/production/main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-housing-production"
+    bucket  = "terraform-state-corporate-production"
     encrypt = true
     region  = "eu-west-2"
-    key     = "services/t-and-l-auth-frontend/state"
+    key     = "services/single-view-auth-frontend/state"
   }
 }
 resource "aws_s3_bucket" "frontend-bucket-production" {
-  bucket = "lbh-housing-tl-auth-frontend-production.hackney.gov.uk"
+  bucket = "lbh-single-view-auth-frontend-production.hackney.gov.uk"
   acl    = "private"
   versioning {
     enabled = true
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "frontend-bucket-production" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
-    allowed_origins = ["https://manage-my-home.hackney.gov.uk"]
+    allowed_origins = ["https://single-view.hackney.gov.uk"]
     expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
     max_age_seconds = 3000
   }
@@ -31,18 +31,18 @@ resource "aws_s3_bucket" "frontend-bucket-production" {
 module "cloudfront-production" {
   source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
   s3_domain_name = aws_s3_bucket.frontend-bucket-production.bucket_regional_domain_name
-  origin_id = "mtfh-t-and-l-auth-frontend"
+  origin_id = "single-view-auth-frontend"
   s3_bucket_arn = aws_s3_bucket.frontend-bucket-production.arn
   s3_bucket_id = aws_s3_bucket.frontend-bucket-production.id
-  orginin_access_identity_desc = "T&L auth frontend cloudfront identity"
+  orginin_access_identity_desc = "Single view auth frontend cloudfront identity"
   cname_aliases = []
   environment_name = "production"
   cost_code= "B0811"
-  project_name= "MTFH Tenants and Leaseholders"
+  project_name= "Single View Frontend"
   use_cloudfront_cert = true
 }
 resource "aws_ssm_parameter" "cdn" {
-  name  = "/housing-tl/production/auth-app-url"
+  name  = "/single-view/production/auth-app-url"
   type  = "String"
   value = "https://${module.cloudfront-production.cloudfront_domain_name}"
   overwrite = true
