45b6b984ad6fcf262ee78cb61ff131811d84950a
el <elena@madetech.com>
2022-03-29 13:43:10 +0100

    deploy single view

---
+++ apps/single-view/terraform/development/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 2.0"
+}
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-corporate-development"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/single-view-frontend/state"
+  }
+}
+resource "aws_s3_bucket" "frontend-bucket-development" {
+  bucket = "lbh-single-view-frontend-development.hackney.gov.uk"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://single-view-development.hackney.gov.uk"]
+    expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
+    max_age_seconds = 3000
+  }
+}
+module "cloudfront-development" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
+  s3_domain_name = aws_s3_bucket.frontend-bucket-development.bucket_regional_domain_name
+  origin_id = "single-view-frontend"
+  s3_bucket_arn = aws_s3_bucket.frontend-bucket-development.arn
+  s3_bucket_id = aws_s3_bucket.frontend-bucket-development.id
+  orginin_access_identity_desc = "Single view frontend cloudfront identity"
+  cname_aliases = []
+  environment_name = "development"
+  cost_code = "B0811"
+  project_name = "Single View"
+  use_cloudfront_cert = true
+  compress = true
+}
+resource "aws_ssm_parameter" "cdn" {
+  name  = "/single-view/development/app-url"
+  type  = "String"
+  value = "https://${module.cloudfront-development.cloudfront_domain_name}"
+  overwrite = true
+}
