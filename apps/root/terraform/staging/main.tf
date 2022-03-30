1f90492d8307684a520147ef1460f17136d88d59
el <elena@madetech.com>
2022-03-30 14:44:05 +0100

    restore root cors rule

--- apps/root/terraform/staging/main.tf
+++ apps/root/terraform/staging/main.tf
@@ -12,8 +12,7 @@ terraform {
 }
 resource "aws_s3_bucket" "frontend-bucket-staging" {
   bucket = "lbh-single-view-root-frontend-staging.hackney.gov.uk"
-  # acl    = "private"
-  acl    = "public-read"
+  acl    = "private"
   versioning {
     enabled = true
   }
@@ -21,13 +20,13 @@ resource "aws_s3_bucket" "frontend-bucket-staging" {
     index_document = "index.html"
     error_document = "error.html"
   }
-  # cors_rule {
-  #   allowed_headers = ["*"]
-  #   allowed_methods = ["GET"]
-  #   allowed_origins = ["https://single-view-staging.hackney.gov.uk"]
-  #   expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
-  #   max_age_seconds = 3000
-  # }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://single-view-staging.hackney.gov.uk"]
+    expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
+    max_age_seconds = 3000
+  }
 }
 module "cloudfront-staging" {
   source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
