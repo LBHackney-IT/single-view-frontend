bf38f6cde86ab02964e157084c669f82daa0c8ac
Charli Wild <charli.wild@charlis-mbp.lan>
2022-04-05 16:32:23 +0100

    add webpack config

--- apps/single-view/webpack.config.js
+++ apps/single-view/webpack.config.js
@@ -30,15 +30,13 @@ module.exports = (webpackConfigEnv, argv) => {
       ],
     },
     externals: ["react-router-dom", "formik", "yup"],
-    plugins: [      
+    plugins: [
       new webpack.EnvironmentPlugin({
-        'process.env': {
-          APP_ENV: process.env.APP_ENV || "development",
-          HOUSING_SEARCH_API_V1: process.env.HOUSING_SEARCH_API_V1 || dotenv.HOUSING_SEARCH_API_V1,
-          PERSON_API_V1: process.env.PERSON_API_V1 || dotenv.PERSON_API_V1,
-          NOTES_API_V1: process.env.NOTES_API_V1 || dotenv.NOTES_API_V1
-        }
-      }),     
+        APP_ENV: process.env.APP_ENV || "development",
+        HOUSING_SEARCH_API_V1: process.env.HOUSING_SEARCH_API_V1 || dotenv.HOUSING_SEARCH_API_V1,
+        PERSON_API_V1: process.env.PERSON_API_V1 || dotenv.PERSON_API_V1,
+        NOTES_API_V1: process.env.NOTES_API_V1 || dotenv.NOTES_API_V1
+      }),
       new ImportMapWebpackPlugin({
         namespace: "@mfe",
         basePath: process.env.APP_CDN || "http://localhost:8005",
