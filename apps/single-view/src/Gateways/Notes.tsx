77cc3961e9d6ed32257959cbb852aa54208830a0
Charli Wild <charli.wild@charlis-mbp.lan>
2022-04-05 14:11:45 +0100

    removed consolelog. added structure for api call

--- apps/single-view/src/Gateways/Notes.tsx
+++ apps/single-view/src/Gateways/Notes.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { getToken } from "../Utils/getHackneyToken";
+import { Note } from "../Interfaces";
 
-export const getNotes = async (id: string): Promise<any> => {
+export const getNotes = async (id: string): Promise<Note[]> => {
   const response = await axios.get(`${process.env.NOTES_API_V1}/notes/${id}`, {
     headers: {
       Authorization: `${getToken()}`,
