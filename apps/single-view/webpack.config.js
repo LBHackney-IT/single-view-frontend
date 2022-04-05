f99a9c2c2ea320c7840460599ccaceb219a7dccd
Charli Wild <charli.wild@charlis-mbp.lan>
2022-04-05 16:07:51 +0100

    syntax update to webpack

--- apps/single-view/webpack.config.js
+++ apps/single-view/webpack.config.js
@@ -36,7 +36,7 @@ module.exports = (webpackConfigEnv, argv) => {
           APP_ENV: process.env.APP_ENV || "development",
           HOUSING_SEARCH_API_V1: process.env.HOUSING_SEARCH_API_V1 || dotenv.HOUSING_SEARCH_API_V1,
           PERSON_API_V1: process.env.PERSON_API_V1 || dotenv.PERSON_API_V1,
-          NOTES_API_V1: process.env.NOTES_API_V1 || dotenv.NOTES_API_V1,
+          NOTES_API_V1: process.env.NOTES_API_V1 || dotenv.NOTES_API_V1
         }
       }),     
       new ImportMapWebpackPlugin({
