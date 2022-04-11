d26ef50631ea3b3336bd9eb725975a29c84e39d4
el <elena@madetech.com>
2022-04-11 12:02:10 +0100

    test search page and tidy up UI

--- apps/single-view/src/Views/SearchView/searchResults.tsx
+++ apps/single-view/src/Views/SearchView/searchResults.tsx
@@ -8,27 +8,33 @@ interface myProps {
 
 export const SearchResults = (props: myProps): JSX.Element => {
   return (
-    <div className="govuk-grid-row" id="searchResults">
+    <div className="govuk-grid-row">
       <div className="govuk-grid-column-two-thirds">
         <h2 className="lbh-heading-h3">{`${props.searchResults.length} results found`}</h2>
-        {props.searchResults.map((person: Person, index: number) => {
-          return (
-            <ul className="lbh-body" key={index}>
-              <a
-                href={`/customers/${person.id}`}
-                className="lbh-link lbh-link--no-visited-state"
-              >
-                {person.firstname} {person.surname}, Date of Birth:{" "}
-                {formatDateOfBirth(person.dateOfBirth)}
-              </a>
-              <div className="lbh-body-s">
-                {person.tenures.map((tenure) => {
-                  return tenure.assetFullAddress;
-                })}
+        <hr />
+        <div id="searchResults">
+          {props.searchResults.map((person: Person, index: number) => {
+            return (
+              <div key={index}>
+                <a
+                  href={`/customers/${person.id}`}
+                  className="lbh-link lbh-link--no-visited-state no-underline"
+                >
+                  {person.firstname} {person.surname}, Date of Birth:{" "}
+                  {formatDateOfBirth(person.dateOfBirth)}
+                </a>
+                <div className="lbh-body-s">
+                  {person.tenures.map((tenure) => {
+                    return tenure.assetFullAddress;
+                  })}
+                  <br />
+                  Person API id: {person.id}
+                </div>
+                <br />
               </div>
-            </ul>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
