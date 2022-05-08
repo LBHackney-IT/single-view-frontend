c6851674974e1d007dce6d8c2d8778288bb92783
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-05-08 19:50:06 +0100

    Refactor notes gateway tests

--- apps/single-view/src/Gateways/Notes.tsx
+++ apps/single-view/src/Gateways/Notes.tsx
@@ -1,57 +1,32 @@
 import axios from "axios";
 import { getToken } from "../Utils/getHackneyToken";
-import { Note, Person } from "../Interfaces";
+import { Note } from "../Interfaces";
 import { $auth } from "@mfe/common/lib/auth";
 
-export const getNotes = async (id: string): Promise<Note[] | null> => {
-  try {
-    const response = await axios.get(
-      `${process.env.NOTES_API_V2}/notes?targetId=${id}`,
-      {
-        headers: {
-          Authorization: `${getToken()}`,
-        },
-      }
-    );
+export const getNotesError = new Error("Error retrieving notes");
+export const createNoteError = new Error("Error creating note");
 
-    if (response.status != 200) {
-      throw new Error("Error retrieving notes");
+export const getNotes = async (systemIds: string): Promise<Note[]> => {
+  const response = await axios.get(
+    `${process.env.SV_API_V1}/notes?systemIds=${systemIds}`,
+    {
+      headers: {
+        Authorization: `${getToken()}`,
+      },
     }
+  );
 
-    return response.data.results;
-  } catch (e) {
-    return null;
+  if (response.status != 200) {
+    throw getNotesError;
   }
-};
 
-export const loadPersonNotes = async (
-  person: Person,
-  collatedNotes: Note[]
-): Promise<Person> => {
-  let result = await getNotes(person.id);
-
-  if (result) {
-    collatedNotes.push(...result);
-  }
-  return person;
-};
-
-export const loadTenureNotes = async (
-  person: Person,
-  collatedNotes: Note[]
-): Promise<void> => {
-  if (person.tenures) {
-    for (const tenure of person.tenures) {
-      let notes = await getNotes(tenure.id);
-      if (notes) collatedNotes.push(...notes);
-    }
-  }
+  return response.data.notes;
 };
 
 export const createNote = async (
   targetId: string,
   data: any
-): Promise<Note | null> => {
+): Promise<Note> => {
   let auth = $auth.getValue();
   let note: Note = Object.assign(
     {
@@ -75,23 +50,15 @@ export const createNote = async (
     data
   );
 
-  try {
-    const response = await axios.post(
-      `${process.env.NOTES_API_V2}/notes`,
-      note,
-      {
-        headers: {
-          Authorization: `${getToken()}`,
-        },
-      }
-    );
+  const response = await axios.post(`${process.env.SV_API_V1}/notes`, note, {
+    headers: {
+      Authorization: `${getToken()}`,
+    },
+  });
 
-    if (response.status != 201) {
-      throw new Error("Error creating note");
-    }
-
-    return response.data;
-  } catch (e) {
-    return null;
+  if (response.status != 201) {
+    throw createNoteError;
   }
+
+  return response.data;
 };
