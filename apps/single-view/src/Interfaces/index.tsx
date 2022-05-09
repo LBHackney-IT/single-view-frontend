6c5667d219dd5ebf9dab2cd1ff0ff80cd6ac871e
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-05-09 15:00:23 +0100

    Add systemIdInterface to exports

--- apps/single-view/src/Interfaces/index.tsx
+++ apps/single-view/src/Interfaces/index.tsx
@@ -1,6 +1,7 @@
 export * from "./housingSearchInterfaces";
 export * from "./notesInterfaces";
 export * from "./personInterfaces";
+export * from "./systemIdInterface";
 
 export interface UrlParams {
   id: string;
