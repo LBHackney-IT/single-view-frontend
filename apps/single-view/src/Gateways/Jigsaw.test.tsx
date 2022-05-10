b11381b2a30514a6f33198e02742942a38de80bd
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-05-10 17:04:42 +0100

    Add Jigsaw gateway

---
+++ apps/single-view/src/Gateways/Jigsaw.test.tsx
@@ -0,0 +1,32 @@
+import { authoriseJigsaw, authoriseJigsawError } from "./Jigsaw";
+import axios from "axios";
+jest.mock("axios", () => {
+  return {
+    get: jest.fn(),
+  };
+});
+
+const mockAxios = axios as jest.Mocked<typeof axios>;
+
+describe("Jigsaw gateway", () => {
+  describe("authoriseJigsaw", () => {
+    it("should return a string if the request is OK", async () => {
+      const token: string = "";
+      const response = { status: 200, data: token };
+      mockAxios.get.mockImplementationOnce(async () => response);
+
+      expect(await authoriseJigsaw("")).toEqual(token);
+    });
+
+    it("should throw an error if the request is not OK", async () => {
+      const response = { status: 401 };
+      mockAxios.get.mockImplementationOnce(async () => response);
+
+      try {
+        await authoriseJigsaw("");
+      } catch (e: any) {
+        expect(e.message).toBe(authoriseJigsawError.message);
+      }
+    });
+  });
+});
---
+++ apps/single-view/src/Gateways/Jigsaw.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { getToken } from "../Utils/getHackneyToken";
+
+export const authoriseJigsawError = new Error("Error authorising with Jigsaw");
+
+export const authoriseJigsaw = async (userName: string): Promise<string> => {
+  const response = await axios.get(
+    `${process.env.SV_API_V1}/authorise?username=test`,
+    {
+      headers: {
+        Authorization: `${getToken()}`,
+      },
+    }
+  );
+
+  if (response.status != 200) {
+    throw authoriseJigsawError;
+  }
+
+  return response.data;
+};
