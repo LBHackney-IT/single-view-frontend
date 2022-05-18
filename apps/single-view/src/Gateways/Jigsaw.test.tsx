d03163a54343865cc8cb1ac68598073fc5246b0b
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-05-18 10:29:12 +0100

    Refactor Jigsaw gateway tests

--- apps/single-view/src/Gateways/Jigsaw.test.tsx
+++ apps/single-view/src/Gateways/Jigsaw.test.tsx
@@ -2,7 +2,7 @@ import { authoriseJigsaw, authoriseJigsawError } from "./Jigsaw";
 import axios from "axios";
 jest.mock("axios", () => {
   return {
-    get: jest.fn(),
+    post: jest.fn(),
   };
 });
 
@@ -13,14 +13,14 @@ describe("Jigsaw gateway", () => {
     it("should return a string if the request is OK", async () => {
       const token: string = "";
       const response = { status: 200, data: token };
-      mockAxios.get.mockImplementationOnce(async () => response);
+      mockAxios.post.mockImplementationOnce(async () => response);
 
       expect(await authoriseJigsaw("", "")).toEqual(token);
     });
 
     it("should throw an error if the request is not OK", async () => {
       const response = { status: 401 };
-      mockAxios.get.mockImplementationOnce(async () => response);
+      mockAxios.post.mockImplementationOnce(async () => response);
 
       try {
         await authoriseJigsaw("", "");
