53c481ac84040200334605b5c75cededc3166917
Charli Wild <charli.wild@charlis-mbp.home>
2022-05-19 11:15:32 +0100

    updated searchResident gateway to query api with firstName & lastName parameters

--- apps/single-view/src/Gateways/SearchResident.tsx
+++ apps/single-view/src/Gateways/SearchResident.tsx
@@ -4,18 +4,20 @@ import { sortResponseByRelevance } from "../Utils/sortResponse";
 import { housingSearchPerson } from "../Interfaces";
 
 export const SearchResident = async (
-  searchParams: string,
+  firstName: string,
+  lastName: string,
   address: string | null,
   page: number
 ): Promise<housingSearchPerson[]> => {
   const response = await axios.get(
-    `${process.env.SV_API_V1}/search?searchText=${searchParams}&page=${page}`,
+    `${process.env.SV_API_V1}/search?firstName=${firstName}&lastName=${lastName}&page=${page}`,
     {
       headers: {
         authorization: `${getToken()}`,
       },
     }
   );
+
   return sortResponseByRelevance(
     response.data.searchResponse.searchResults,
     address
