ddae3bbe16885b0142fea5b59b3fd4d040527bba
el <elena@madetech.com>
2022-05-23 11:40:26 +0100

    write better tests

--- apps/single-view/src/Utils/security.test.tsx
+++ apps/single-view/src/Utils/security.test.tsx
@@ -1,24 +1,30 @@
 import { encrypt } from "./security";
 
+const mockSetKey = jest.fn();
+const mockEncrypt = jest.fn();
+
+jest.mock("jsencrypt", () => {
+  return jest.fn(() => {
+    return {
+      setKey: mockSetKey,
+      encrypt: mockEncrypt,
+    };
+  });
+});
+
 describe("security", () => {
   describe("encrypt", () => {
     it("should return a string", () => {
-      const key =
-        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz1xzhzn9ulK+rQ0gOCb/\n" +
-        "xzquQgelF8BGtyXRiMqs0hLbwDSGsmNm27l7+ZJ4FyUEWz7WBQdpXg7GKgqU4JOo\n" +
-        "5z/rS5L2CtYxMv09G0JL28GSeCKbmKyc0FU7XXuFXT52E6MAHAEX8A8It7PkQxnY\n" +
-        "n7GF0T5BOzrTE/lJGvrZ9SNbvTholxthdy7EyNSoTGwqplc64xLK/puEWnDqBeXs\n" +
-        "cPBKUnqkTCtjCi8v16f+u5qQ3Pvo6IWq8BhPLP0OttTG1dGm8hT0V8oRsXs1ueU6\n" +
-        "bdGySzcZhHHraidNjR0DD0cCGj5wtJv6uGJt5IjTwtgkwqXzC1PjgbreLSnmESqx\n" +
-        "bwIDAQAB";
+      const key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz1xzhzn9ulK";
+      const textToEncrypt = '{"username": "testUser", "password": "pa$$w0rd"}';
 
-      const encryptedText = encrypt(
-        '{"username": "testUser", "password": "pa$$w0rd"}',
-        key
-      );
-      expect(encryptedText).toBe(
-        "e9AfdDArc1+Bw/D9eBAsT4WBrUc3VSjnUIgdDa+NrwFjwIYUyupVfxDXjy8Ju0LPNlIcxDlmL6AvC2PlIcJ1h0WvAiaX9SG4C7mu+mYeCb/bJjJKIOScDCaNrvdRiFcwDs3azjXB2S4N5efPRauv7NGaZDxyllVv0sJwNMt9BVPYkvanXnmRIYFm15YSPI7qYB+VmC1xmoLSdpeBQFOhT6B90kGSFK3ZUcc2xhzkNJbpfSHqOeb8jG/xMD6Lk97O1kF0dbZFhfUHwCAcnwNJCYJ0SxqSIc7JGt/Xg0Nx2sD929YDgst6L/nZ/DYuZnqAHmT5Zv8wL1/ZCjlXhb7rxw=="
-      );
+      encrypt(textToEncrypt, key);
+
+      expect(mockSetKey.mock.calls.length).toBe(1);
+      expect(mockSetKey.mock.calls[0][0]).toBe(key);
+
+      expect(mockEncrypt.mock.calls.length).toBe(1);
+      expect(mockEncrypt.mock.calls[0][0]).toBe(textToEncrypt);
     });
   });
 });
