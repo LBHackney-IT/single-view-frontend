b7b82521778a6a7d1e3054ed98cff5902d1cb690
Charli Wild <charli.wild@charlis-mbp.lan>
2022-06-09 17:10:04 +0100

    added selecte property to interface

--- apps/single-view/src/Interfaces/housingSearchInterfaces.tsx
+++ apps/single-view/src/Interfaces/housingSearchInterfaces.tsx
@@ -12,6 +12,7 @@ export interface housingSearchPerson {
   IsPersonCautionaryAlerted: boolean;
   IsTenureCautionaryAlerted: boolean;
   knownAddresses: knownAddress[];
+  isSelected: boolean;
 }
 
 export interface knownAddress {
