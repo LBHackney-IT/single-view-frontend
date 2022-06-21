90007c84ed221f087303c4d28c8d7ffa960cd927
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-06-21 16:00:02 +0100

    Refactor notes integration tests

--- cypress/integration/6-notes.spec.ts
+++ cypress/integration/6-notes.spec.ts
@@ -1,21 +1,23 @@
 import { AuthRoles } from '../support/commands';
 
 describe('Notes', () => {
-  before(() => {
-    cy.intercept('GET', '**/getPersonApiCustomer*', { fixture: 'person-profile.json' }).as('getPerson');
-    cy.intercept('GET', '**/notes*', { fixture: 'customer-notes.json' }).as('getNotes');
-    cy.visitAs('/customers/PersonAPI/6d7ed1a4#notes', AuthRoles.UnrestrictedGroup);
-  });
+  describe("Displays notes", () => {
+    before(() => {
+      cy.intercept('GET', '**/getPersonApiCustomer*', { fixture: 'person-profile.json' }).as('getPerson');
+      cy.intercept('GET', '**/notes*', { fixture: 'customer-notes.json' }).as('getNotes');
+      cy.visitAs('/customers/PersonAPI/6d7ed1a4#notes', AuthRoles.UnrestrictedGroup);
+    });
 
-  it('displays the notes tab', () => {
-    cy.get('#notes', { timeout: 10000 })
-      .should('be.visible')
-  });
+    it('displays the notes tab', () => {
+      cy.get('#notes', { timeout: 10000 })
+        .should('be.visible')
+    });
 
-  it('displays a snapshot', () => {
-    cy.get('[data-snapshot="1"]').should('be.visible', {timeout: 10000});
-    cy.get('[data-testid^="note_"]:visible'). should('have.length', 5);
-  });
+    it('displays a snapshot', () => {
+      cy.get('[data-snapshot="1"]').should('be.visible', {timeout: 10000});
+      cy.get('[data-testid^="note_"]:visible'). should('have.length', 5);
+    });
 
-  // TODO: Add tests of note display
-})
+    // TODO: Add tests of note display
+  });
+});
