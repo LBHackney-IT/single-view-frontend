51257ae83d301a59a26fb40c5f2c347a1e76252e
Tausif <tausifpatel.dev@gmail.com>
2022-07-05 15:39:53 +0100

    Remove bug to grey out labels for empty strings

--- apps/single-view/src/Components/DescriptionListItem.tsx
+++ apps/single-view/src/Components/DescriptionListItem.tsx
@@ -6,12 +6,15 @@ interface Props {
 }
 
 export const DescriptionListItem: React.FC<Props> = (props) => {
-  let isNull: boolean = props.children == null;
+  let isNullOrEmpty: boolean = props.children == null || props.children == "";
+
   return (
     <div className="govuk-summary-list__row">
       <dt className="govuk-summary-list__key govuk-!-width-one-half">
         <h5
-          className={isNull ? "lbh-heading-h5 sv-null-field" : "lbh-heading-h5"}
+          className={
+            isNullOrEmpty ? "lbh-heading-h5 sv-null-field" : "lbh-heading-h5"
+          }
         >
           {props.title}
         </h5>
