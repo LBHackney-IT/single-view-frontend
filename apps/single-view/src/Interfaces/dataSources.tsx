97e91e4e6d2cee7f06fb01f755c3a8dde2359e88
Charli Wild <charli.wild@Charlis-MacBook-Pro.local>
2022-07-07 11:22:02 +0100

    refactored person gateway and added AcademyCT datasource

--- apps/single-view/src/Interfaces/dataSources.tsx
+++ apps/single-view/src/Interfaces/dataSources.tsx
@@ -2,3 +2,4 @@ export const Jigsaw = "Jigsaw";
 export const Housing = "PersonAPI";
 export const AcademyCT = "Academy-CouncilTax";
 export const Benefits = "Academy-Benefits";
+export const SingleView = "single-view";
