273ee409ad6f3b162a96b4b46a7d17d6197ebea8
Charli Wild <charli.wild@charlis-mbp.lan>
2022-07-21 12:41:13 +0100

    added error handling to component

--- apps/single-view/src/Views/CustomerView/Cases.tsx
+++ apps/single-view/src/Views/CustomerView/Cases.tsx
@@ -1,4 +1,4 @@
-import { Center, Spinner } from "@mfe/common/lib/components";
+import { Center, ErrorSummary, Spinner } from "@mfe/common/lib/components";
 import React, { useEffect, useState } from "react";
 import { getCasesByCustomerId } from "../../Gateways/JigsawCases";
 import { getCookie } from "../../Utils";
@@ -11,18 +11,18 @@ interface Props {
 
 export const Cases = (props: Props): JSX.Element => {
   const [cases, setCases] = useState<jigsawCasesResponse>();
+  const [getCasesError, setGetCasesError] = useState<boolean>(false);
 
   const loadCases = async (customerId: string): Promise<void> => {
-    let cases = null;
-
-    if (!customerId) {
-      return;
-    }
-
+    setGetCasesError(false);
     try {
-      cases = await getCasesByCustomerId(customerId, getCookie("jigsawToken"));
+      let cases = await getCasesByCustomerId(
+        customerId,
+        getCookie("jigsawToken")
+      );
       setCases(cases);
     } catch (e: any) {
+      setGetCasesError(true);
       console.log(`Error is ${e.message}`);
     }
   };
@@ -33,6 +33,16 @@ export const Cases = (props: Props): JSX.Element => {
     }
   }, [props.customerId]);
 
+  if (getCasesError) {
+    return (
+      <ErrorSummary
+        id="singleViewNotesError"
+        title="Error"
+        description="Unable to load cases"
+      />
+    );
+  }
+
   if (typeof cases == "undefined") {
     return (
       <Center>
@@ -42,7 +52,7 @@ export const Cases = (props: Props): JSX.Element => {
   }
 
   {
-    return cases.currentCase != null ? (
+    return cases != null ? (
       <CaseSummary jigsawCaseResponse={cases} />
     ) : (
       <div className="govuk-inset-text lbh-inset-text" data-testid="notFound">
