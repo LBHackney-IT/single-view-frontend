8829f42ccd0f447a6014a9fc8fdc7853295d460b
el <elena@madetech.com>
2022-08-04 10:16:49 +0100

    restore profile page

--- apps/single-view/src/Views/CustomerView/Profile.tsx
+++ apps/single-view/src/Views/CustomerView/Profile.tsx
@@ -54,191 +54,157 @@ export const Profile = (props: Props) => {
             />
           );
         })}
-      <main className="govuk-main-wrapper">
-        <div className="govuk-grid-row">
-          <div className="govuk-grid-column-one-half">
-            <dl className="govuk-summary-list lbh-summary-list">
-              <DescriptionListItem title="Name" testId="name">
-                {person.title} {person.firstName} {person.surname}
-              </DescriptionListItem>
 
-              {/* Display preferred title+name only if it doesn't match the default title+name */}
-              {((person.preferredTitle &&
-                person.preferredTitle != person.title) ||
-                (person.preferredFirstName &&
-                  person.preferredFirstName != person.firstName) ||
-                (person.preferredSurname &&
-                  person.preferredSurname != person.surname)) && (
-                <DescriptionListItem
-                  title="Preferred Name"
-                  testId="preferredName"
-                >
-                  {(person.preferredTitle
-                    ? person.preferredTitle
-                    : person.title) + " "}
-                  {(person.preferredFirstName
-                    ? person.preferredFirstName
-                    : person.firstName) + " "}
-                  {person.preferredSurname
-                    ? person.preferredSurname
-                    : person.surname}
-                </DescriptionListItem>
-              )}
+      <dl className="govuk-summary-list lbh-summary-list">
+        <DescriptionListItem title="Name" testId="name">
+          {person.title} {person.firstName} {person.surname}
+        </DescriptionListItem>
 
-              {person.dateOfBirth && (
-                <DescriptionListItem title="Date of Birth" testId="dateOfBirth">
-                  {formatDateOfBirth(person.dateOfBirth)}
-                </DescriptionListItem>
-              )}
+        {/* Display preferred title+name only if it doesn't match the default title+name */}
+        {((person.preferredTitle && person.preferredTitle != person.title) ||
+          (person.preferredFirstName &&
+            person.preferredFirstName != person.firstName) ||
+          (person.preferredSurname &&
+            person.preferredSurname != person.surname)) && (
+          <DescriptionListItem title="Preferred Name" testId="preferredName">
+            {(person.preferredTitle ? person.preferredTitle : person.title) +
+              " "}
+            {(person.preferredFirstName
+              ? person.preferredFirstName
+              : person.firstName) + " "}
+            {person.preferredSurname ? person.preferredSurname : person.surname}
+          </DescriptionListItem>
+        )}
 
-              <DescriptionListItem
-                title="Contact Details"
-                testId="contactDetails"
-              >
-                {person.allContactDetails &&
-                  person.allContactDetails.map((contactDetail, index) => {
-                    return (
-                      <div key={index} className="contact-detail">
-                        <p>
-                          <span
-                            data-testid="contactDetailsContactType"
-                            className="govuk-!-font-weight-bold"
-                          >
-                            {contactDetail.contactType} ({contactDetail.subType}
-                            )
-                          </span>
-                          <span data-testid="contactDetailsDescription">
-                            {contactDetail.description &&
-                              " - " + contactDetail.description + ":"}
-                          </span>
-                          <br />
-                          <span data-testid="contactDetailsValue">
-                            {contactDetail.value}
-                          </span>
-                          <br />
-                          <span data-testid="contactDetailsDataSource">
-                            <span className="govuk-!-font-weight-bold">
-                              Data Source:{" "}
-                            </span>
-                            {contactDetail.dataSourceName}
-                          </span>
-                        </p>
-                      </div>
-                    );
-                  })}
-              </DescriptionListItem>
+        {person.dateOfBirth && (
+          <DescriptionListItem title="Date of Birth" testId="dateOfBirth">
+            {formatDateOfBirth(person.dateOfBirth)}
+          </DescriptionListItem>
+        )}
 
-              <DescriptionListItem title="Tenures" testId="tenures">
-                {person.knownAddresses?.map((address, index) => {
-                  return (
-                    <p className="lbh-body-s" key={index}>
-                      <span data-testid="tenureFullAddress">
-                        {address.fullAddress}
-                      </span>
-                      <br />
-                      <span data-testid="tenureStartDate">
-                        <span className="govuk-!-font-weight-bold">
-                          Start date:
-                        </span>{" "}
-                        {formatDate(address.startDate) || "N/A"}
-                      </span>
-                      <br />
-                      <span data-testid="tenureEndDate">
-                        <span className="govuk-!-font-weight-bold">
-                          End date:
-                        </span>{" "}
-                        {formatDate(address.endDate) || "N/A"}
-                      </span>
-                      <br />
-                      <span data-testid="tenureDataSource">
-                        <span className="govuk-!-font-weight-bold">
-                          Data Source:
-                        </span>{" "}
-                        {address.dataSourceName}
-                      </span>
-                    </p>
-                  );
-                })}
-              </DescriptionListItem>
-              <DescriptionListItem title="Types" testId="types">
-                {person.personTypes?.join(", ")}
-              </DescriptionListItem>
-              <DescriptionListItem
-                title="National Insurance Number"
-                testId="nino"
-              >
-                {person.niNo}
-              </DescriptionListItem>
-              <DescriptionListItem title="Place of Birth" testId="placeOfBirth">
-                {person.placeOfBirth}
-              </DescriptionListItem>
-              <DescriptionListItem title="NHS number" testId="nhsNo">
-                {person.nhsNumber}
-              </DescriptionListItem>
-              <DescriptionListItem
-                title={"Pregnancy due Date"}
-                testId={"pregnancyDueDate"}
-              >
-                {person.pregnancyDueDate &&
-                  formatDateOfBirth(person.pregnancyDueDate)}
-              </DescriptionListItem>
-              <DescriptionListItem
-                title={"Accommodation type Id"}
-                testId={"accommodationTypeId"}
-              >
-                {person.accommodationTypeId}
-              </DescriptionListItem>
-              <DescriptionListItem
-                title={"Housing circumstance Id"}
-                testId={"housingCircumstanceId"}
-              >
-                {person.housingCircumstanceId}
-              </DescriptionListItem>
-              <DescriptionListItem title={"Settled"} testId={"isSettled"}>
-                {person.isSettled ? "Y" : "N"}
-              </DescriptionListItem>
-              <DescriptionListItem
-                title={"Support worker"}
-                testId={"supportWorker"}
-              >
-                {person.supportWorker}
-              </DescriptionListItem>
-              <DescriptionListItem title={"Gender"} testId={"gender"}>
-                {person.gender}
-              </DescriptionListItem>
-              <DescriptionListItem title="Is a Minor" testId="isMinor">
-                {person.isAMinor ? "Y" : "N"}
-              </DescriptionListItem>
-              <DescriptionListItem title="Date of Death" testId="dateOfDeath">
-                {person.dateofDeath}
-              </DescriptionListItem>
-            </dl>
-          </div>
-          <div className="govuk-grid-column-one-half">
-            <dl className="govuk-summary-list lbh-summary-list">
-              <h3>System Ids</h3>
-              {systemIds &&
-                systemIds.map((systemId) => {
-                  return (
-                    <DescriptionListItem
-                      title={systemId.systemName}
-                      key={systemId.id}
-                      testId={systemId.systemName}
+        <DescriptionListItem title="Contact Details" testId="contactDetails">
+          {person.allContactDetails &&
+            person.allContactDetails.map((contactDetail, index) => {
+              return (
+                <div key={index} className="contact-detail">
+                  <p>
+                    <span
+                      data-testid="contactDetailsContactType"
+                      className="govuk-!-font-weight-bold"
                     >
-                      {systemId.id}
-                    </DescriptionListItem>
-                  );
-                })}
-              <CouncilTaxInformation
-                councilTaxAccount={person.councilTaxAccount}
-              />
-              <HousingBenefitsInformation
-                housingBenefitsAccount={person.housingBenefitsAccount}
-              />
-            </dl>
-          </div>
-        </div>
-      </main>
+                      {contactDetail.contactType} ({contactDetail.subType})
+                    </span>
+                    <span data-testid="contactDetailsDescription">
+                      {contactDetail.description &&
+                        " - " + contactDetail.description + ":"}
+                    </span>
+                    <br />
+                    <span data-testid="contactDetailsValue">
+                      {contactDetail.value}
+                    </span>
+                    <br />
+                    <span data-testid="contactDetailsDataSource">
+                      <span className="govuk-!-font-weight-bold">
+                        Data Source:{" "}
+                      </span>
+                      {contactDetail.dataSourceName}
+                    </span>
+                  </p>
+                </div>
+              );
+            })}
+        </DescriptionListItem>
+
+        <DescriptionListItem title="Tenures" testId="tenures">
+          {person.knownAddresses?.map((address, index) => {
+            return (
+              <p className="lbh-body-s" key={index}>
+                <span data-testid="tenureFullAddress">
+                  {address.fullAddress}
+                </span>
+                <br />
+                <span data-testid="tenureStartDate">
+                  <span className="govuk-!-font-weight-bold">Start date:</span>{" "}
+                  {formatDate(address.startDate) || "N/A"}
+                </span>
+                <br />
+                <span data-testid="tenureEndDate">
+                  <span className="govuk-!-font-weight-bold">End date:</span>{" "}
+                  {formatDate(address.endDate) || "N/A"}
+                </span>
+                <br />
+                <span data-testid="tenureDataSource">
+                  <span className="govuk-!-font-weight-bold">Data Source:</span>{" "}
+                  {address.dataSourceName}
+                </span>
+              </p>
+            );
+          })}
+        </DescriptionListItem>
+        <DescriptionListItem title="Types" testId="types">
+          {person.personTypes?.join(", ")}
+        </DescriptionListItem>
+        <DescriptionListItem title="National Insurance Number" testId="nino">
+          {person.niNo}
+        </DescriptionListItem>
+        <DescriptionListItem title="Place of Birth" testId="placeOfBirth">
+          {person.placeOfBirth}
+        </DescriptionListItem>
+        <DescriptionListItem title="NHS number" testId="nhsNo">
+          {person.nhsNumber}
+        </DescriptionListItem>
+        <DescriptionListItem
+          title={"Pregnancy due Date"}
+          testId={"pregnancyDueDate"}
+        >
+          {person.pregnancyDueDate &&
+            formatDateOfBirth(person.pregnancyDueDate)}
+        </DescriptionListItem>
+        <DescriptionListItem
+          title={"Accommodation type Id"}
+          testId={"accommodationTypeId"}
+        >
+          {person.accommodationTypeId}
+        </DescriptionListItem>
+        <DescriptionListItem
+          title={"Housing circumstance Id"}
+          testId={"housingCircumstanceId"}
+        >
+          {person.housingCircumstanceId}
+        </DescriptionListItem>
+        <DescriptionListItem title={"Settled"} testId={"isSettled"}>
+          {person.isSettled ? "Y" : "N"}
+        </DescriptionListItem>
+        <DescriptionListItem title={"Support worker"} testId={"supportWorker"}>
+          {person.supportWorker}
+        </DescriptionListItem>
+        <DescriptionListItem title={"Gender"} testId={"gender"}>
+          {person.gender}
+        </DescriptionListItem>
+        <DescriptionListItem title="Is a Minor" testId="isMinor">
+          {person.isAMinor ? "Y" : "N"}
+        </DescriptionListItem>
+        <DescriptionListItem title="Date of Death" testId="dateOfDeath">
+          {person.dateofDeath}
+        </DescriptionListItem>
+        <h3>System Ids</h3>
+        {systemIds &&
+          systemIds.map((systemId) => {
+            return (
+              <DescriptionListItem
+                title={systemId.systemName}
+                key={systemId.id}
+                testId={systemId.systemName}
+              >
+                {systemId.id}
+              </DescriptionListItem>
+            );
+          })}
+        <CouncilTaxInformation councilTaxAccount={person.councilTaxAccount} />
+        <HousingBenefitsInformation
+          housingBenefitsAccount={person.housingBenefitsAccount}
+        />
+      </dl>
     </>
   );
 };
