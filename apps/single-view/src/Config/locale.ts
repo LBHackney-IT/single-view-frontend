9db068486a259a1247a9481adb286eda06c649df
Adam <adam.tracy@hackney.gov.uk>
2022-10-06 14:29:05 +0100

    Updated comments on config locale

--- apps/single-view/src/Config/locale.ts
+++ apps/single-view/src/Config/locale.ts
@@ -4,9 +4,9 @@ export const locale = {
     jigsawLogin: "Jigsaw Login", // /jigsawLogin
     search: "Search", // /search
     personProfile: {
-      profile: "Profile",
-      notes: "Notes",
-      cases: "Cases",
+      profile: "Profile", // #profile
+      notes: "Notes", // #notes
+      cases: "Cases", // #cases
     },
   },
 };
