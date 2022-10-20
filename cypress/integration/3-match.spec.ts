f10143b8d66960ce48629278b9e5f7709af506db
Adam <adam.tracy@hackney.gov.uk>
2022-10-20 10:01:32 +0100

    Revert "Revert config.yml from main"
    This reverts commit e43eaa0f5356e35dde7630c85d8fca31104912c4.

--- cypress/integration/3-match.spec.ts
+++ cypress/integration/3-match.spec.ts
@@ -3,12 +3,13 @@ import { searchPage } from '../pages/search-page';
 
 describe('matching', () => {
 	before(() => {
-		cy.intercept('GET', '**/search?**', { fixture: 'person-search.json' }).as('getPersons')
+		searchPage.pageUrl = "/search?firstName=Luna&lastName=Kitty"
 		searchPage.visit(AuthRoles.UnrestrictedGroup, JigsawStatuses.LoggedIn)
-	})
+		searchPage.search("Luna", "Kitty")
+	});
 
 	it('allows user to match results', () => {
-		searchPage.search("Luna", "Kitty")
+		cy.intercept('GET', '**/search?**', { fixture: 'person-search.json' }).as('getPersons')
 		searchPage.elements.getMatchButton()
 			.should('be.disabled');
 
