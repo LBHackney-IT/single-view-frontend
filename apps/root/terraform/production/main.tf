3bf6a479321926c81b3540d2329518cbfe734a5c
Selwyn Preston <Selwyn.Preston@hackney.gov.uk>
2024-10-21 15:56:32 +0100

    use correct quotes

--- apps/root/terraform/production/main.tf
+++ apps/root/terraform/production/main.tf
@@ -29,7 +29,7 @@ module "cloudfront-production" {
   s3_bucket_arn = aws_s3_bucket.frontend-bucket-production.arn
   s3_bucket_id = aws_s3_bucket.frontend-bucket-production.id
   orginin_access_identity_desc = "Single View root frontend cloudfront identity"
-  cname_aliases = ['single-view.hackney.gov.uk'] # add alias in
+  cname_aliases = ["single-view.hackney.gov.uk"]
   environment_name = "production"
   cost_code = "B0811"
   project_name = "Single View"
